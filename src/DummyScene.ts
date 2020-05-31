62012c3f8240d13bc48bc348bba20a2a7e83204f
Soma Sörös <soma.soros@dpc.hu>
2020-05-31 16:28:39 +0200

    add scale option for layers

--- src/DummyScene.ts
+++ src/DummyScene.ts
@@ -1,7 +1,7 @@
 import {Scene} from './Scene';
 import {Layer} from './Layer';
 import {Game} from './Game';
-import {ImageSprite, SerializableImageSpriteDescriptor} from './ImageSprite';
+import {ImageSprite} from './ImageSprite';
 
 const RANDOM_ESNW = ['E', 'S', 'N', 'W'];
 
@@ -17,8 +17,8 @@ export class DummyScene extends Scene {
     spriteList = (): string[] => ['assets/dirt.json'];
 
     init = (): void => {
-        const dummyLayer = new Layer(0, 0, Game.getInstance().width, Game.getInstance().height);
-        for (let i = 0; i < 20; i++) {
+        const dummyLayer = new Layer(0, 0, Game.getInstance().width, Game.getInstance().height, .20);
+        for (let i = 0; i < 40; i++) {
             for (let j = 0; j < 20; j++) {
                 dummyLayer.sprites.push(new ImageSprite('dirt_' + randomENSW(), 256 * j + 128 * (i % 2), 64 * i));
             }
--- src/Layer.ts
+++ src/Layer.ts
@@ -1,18 +1,20 @@
-import {Container} from "./Container";
-import {RenderEvent} from "./RenderEvent";
-import {Rectangle} from "./Rectangle";
+import {Container} from './Container';
+import {RenderEvent} from './RenderEvent';
+import {Rectangle} from './Rectangle';
 
 export class Layer extends Container implements Rectangle {
 
     constructor(public x: number,
                 public y: number,
                 public w: number,
-                public h: number) {
+                public h: number, public scale = 1) {
         super();
     }
 
     render = (event: RenderEvent) => {
+        event.context.scale(this.scale, this.scale);
         this.renderSprites(event);
+        event.context.resetTransform();
     };
 
 
