27749dffcd3680a213eca26857a1f2651ce26b25
Soma Sörös <soma.soros@dpc.hu>
2020-09-03 18:39:20 +0200

    create visibility flag for layers

--- src/Layer.ts
+++ src/Layer.ts
@@ -4,6 +4,7 @@ import {Rectangle} from './Rectangle';
 import {UpdateEvent} from './UpdateEvent';
 
 export class Layer extends Container implements Rectangle {
+    visible = true;
 
     constructor(public x: number,
                 public y: number,
@@ -13,12 +14,14 @@ export class Layer extends Container implements Rectangle {
     }
 
     update(event: UpdateEvent): void {
+        if (!this.visible) return;
         event.mouseState.x = (-this.x * this.scale + event.mouseState.nativeXCoordinate) / this.scale;
         event.mouseState.y = (-this.y * this.scale + event.mouseState.nativeYCoordinate) / this.scale;
         this.updateSprites(event);
     }
 
     render(event: RenderEvent) {
+        if (!this.visible) return;
         event.context.scale(this.scale, this.scale);
         event.context.translate(this.x, this.y);
         this.renderSprites(event);
